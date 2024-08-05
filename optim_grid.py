4347b6f4fd474b6298ea3dad33465706e20c6ab0
anatrini <134724389+anatrini@users.noreply.github.com>
2024-08-05 13:03:45 +0200

    update optim_grid
    update optim_grid

--- optim_grid.py
+++ optim_grid.py
@@ -1,3 +1,4 @@
+import argparse
 import itertools
 import numpy as np
 import torch
@@ -11,39 +12,45 @@ from sklearn.model_selection import train_test_split
 from utils import *
 
 
+def get_arguments():
+    parser = argparse.ArgumentParser()
+
+    # Small dataset of the presets to be reduced (Mandatory)
+    parser.add_argument('-f', '--filepath',
+                      dest='filepath',
+                      type=str,
+                      default=None)
+    
+    # Large dataset of presets to pretrain the model (Optional)
+    parser.add_argument('-F', '--filepath_pretrain',
+                      dest='filepath_pretrain',
+                      type=str,
+                      default=None)
+    
+    return parser.parse_args()
+
+
 logging = setup_logger('Grid Opmization session', file=True)
-torch.manual_seed(42)
+
 
 # Load data
-filepath = './data/rendered_presets_dataset.csv'
-loader = DataLoader(filepath)
-df = loader.load_presets()
+def load_data(filepath):
+    loader = DataLoader(filepath)
+    return loader.load_presets()
 
-df_train, df_test = train_test_split(df, test_size=0.2, random_state=42)
-
-
-# VAE's params' grid
-vae_grid = {
-    'n_epochs': [5, 10, 25, 50, 75, 100, 150, 200],
-    'learning_rate': np.logspace(-5, -2, num=4),
-    'weight_decay': np.logspace(-5, -2, num=4),
-    'n_layers': list(range(1, 6)),
-    'activation': ['ReLU', 'Sigmoid', 'Tanh'],
-    'beta': np.linspace(0.1, 1.0, num=10)
-}
-
-# Get all combinations of hyperparameters
-param_combinations = list(itertools.product(*vae_grid.values()))
 
 # Compute KL divergence
 def kl_divergence(mu, logvar):
     return -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
 
-def compute_validation_error(model, criterion, data, beta):
-    #data = data.drop(columns=['ID', 'PRESET_NAME'])
-    data = data.drop(columns=['ID', 'name', 'file'])
 
+# Calculate validation error
+def compute_validation_error(model, criterion, data, beta):
+    
+    # Drop unnecessary columns
+    data = data.drop(columns=['ID', 'name', 'file'])
     data = torch.tensor(data.values).float()
+
     with torch.no_grad():
         mu, logvar, output = model(data)
         recon_loss = criterion(output, data)
@@ -52,89 +59,139 @@ def compute_validation_error(model, criterion, data, beta):
         loss = (recon_loss + (kl_loss * beta)) + mse_loss
     return loss.item()
 
-best_validation_error = float('inf')
-best_params = None
 
-# Loop over all combinations of hyperparameters
-for i, params in enumerate(param_combinations):
-    # unpack params
-    n_epochs, learning_rate, weight_decay, n_layers, activation_name, beta = params
+def optimize_vae(df_train, df_test, log_prefix):
+    
+    # VAE's params' grid
+    vae_grid = {
+        'n_epochs': [5, 10, 25, 50, 75, 100, 150, 200],
+        'learning_rate': np.logspace(-5, -2, num=4),
+        'weight_decay': np.logspace(-5, -2, num=4),
+        'n_layers': list(range(1, 6)),
+        'activation': ['ReLU', 'Sigmoid', 'Tanh'],
+        'beta': np.linspace(0.1, 1.0, num=10)
+    }
+
+    # Get all combinations of hyperparameters
+    param_combinations = list(itertools.product(*vae_grid.values()))
+    best_validation_error = float('inf')
+    best_params = None
+
+    # Loop over all combinations of hyperparameters
+    for i, params in enumerate(param_combinations):
+        # unpack params
+        n_epochs, learning_rate, weight_decay, n_layers, activation_name, beta = params
+        activation = get_activation_function(activation_name)
+
+        # train the model
+        reducer = VectorReducer(df_train, learning_rate, weight_decay, n_layers, activation, beta)
+        reducer.train_vae(n_epochs)
+
+        # compute validation error
+        validation_error = compute_validation_error(reducer.model, reducer.criterion, df_test, beta)
+        print(f'{log_prefix} trial {i+1}/{len(param_combinations)}: validation_error = {validation_error}')
+
+        # update best parameters if current model is better
+        if validation_error < best_validation_error:
+            best_validation_error = validation_error
+            best_params = params
+
+
+    # Save best VAE params and log the best hyperparameters and validation error
+    logging.info(f'{log_prefix} best VAE hyperparams: {best_params} with a validation error of {best_validation_error}')
+    return best_params
+
+
+def optimize_interpolator(df, reducer, log_prefix):
+
+    # Interpolator's params' grid
+    interpolator_grid = {
+        'smoothing': np.linspace(0.001, 1.0, num=10),
+        'kernel': ['multiquadric', 'inverse_multiquadric', 'inverse_quadratic', 'gaussian', 'linear', 'quintic', 'cubic', 'thin_plate_spline'],
+        'epsilon': np.linspace(0.0, 3.0, num=30)
+    }
+
+    param_combinations = list(itertools.product(*interpolator_grid.values()))
+    best_validation_distance = float('inf')
+    best_params = None
+
+    # Loop over all combinations of hyperparameters
+    for i, params in enumerate(param_combinations):
+        # unpack params
+        smoothing, kernel, epsilon = params
+
+        # train the model
+        original_data = df.drop(['ID', 'name', 'file'], axis=1)
+        original_data = original_data.values
+        reduced_data, _ = reducer.vae()
+        reduced_data = reduced_data[:, 1:]
+        
+        # train the interpolator
+        interpolator = RBFInterpolator(reduced_data, original_data, smoothing=smoothing, kernel=kernel, epsilon=epsilon)
+        interpolated_data = interpolator(reduced_data)
+
+        # Compute euclidean distance between original and reduced vectors
+        distances = []
+        for original, interpolated in zip(original_data, interpolated_data):
+            distance = euclidean(original, interpolated)
+            distances.append(distance)
+
+        validation_distance = np.mean(distances)
+        print(f'{log_prefix} trial {i+1}/{len(param_combinations)}: validation_distance = {validation_distance}')
+
+        if validation_distance < best_validation_distance:
+            best_validation_distance = validation_distance
+            best_params = params
+
+    # Save best VAE params and log the best hyperparameters and validation error
+    logging.info(f'{log_prefix} best RBF params: {best_params} with a validation error of {best_validation_distance}')
+    return best_params
+
+
+def main():
+    args = get_arguments()
+    torch.manual_seed(42)
+
+    filepath = args.filepath
+    filepath_pretrain = args.filepath_pretrain
+
+    df = load_data(filepath)
+    df_train, df_test = train_test_split(df, test_size=0.2, random_state=42)
+
+    if filepath_pretrain:
+        # Optimize and train the VAE with best pretrain params
+        df_pretrain = load_data(filepath_pretrain)
+        df_pretrain_train, df_pretrain_test = train_test_split(df_pretrain, test_size=0.2, random_state=42)
+
+        best_pretrain_params = optimize_vae(df_pretrain_train, df_pretrain_test, 'Pretrain')
+        n_epochs, learning_rate, weight_decay, n_layers, activation_name, beta = best_pretrain_params
+        activation = get_activation_function(activation_name)
+        reducer = VectorReducer(df_pretrain, learning_rate, weight_decay, n_layers, activation, beta)
+        reducer.train_vae(n_epochs)
+
+        best_train_params = optimize_vae(df_train, df_test, 'Train')
+
+    else:
+        best_train_params = optimize_vae(df_train, df_test, 'Train')
+
+    # Train the VAE with the best train params
+    n_epochs, learning_rate, weight_decay, n_layers, activation_name, beta = best_train_params
     activation = get_activation_function(activation_name)
-
-    # train the model
     reducer = VectorReducer(df_train, learning_rate, weight_decay, n_layers, activation, beta)
     reducer.train_vae(n_epochs)
 
-    # compute validation error
-    validation_error = compute_validation_error(reducer.model, reducer.criterion, df_test, beta)
-    print(f'Trial {i+1}/{len(param_combinations)}: validation_error = {validation_error}')
+    # Train the interpolator with the best train params
+    best_rbf_params = optimize_interpolator(df, reducer, 'Interpolator')
 
-    # update best parameters if current model is better
-    if validation_error < best_validation_error:
-        best_validation_error = validation_error
-        best_params = params
-
-
-# # Save best VAE params and log the best hyperparameters and validation error
-best_vae_params = best_params
-logging.info(f'Best VAE hyperparams: {best_vae_params} with a validation error of {best_validation_error}')
-
-# Train the VAE with the best params
-n_epochs, learning_rate, weight_decay, n_layers, activation_name, beta = best_vae_params
-activation = get_activation_function(activation_name)
-reducer = VectorReducer(df, learning_rate, weight_decay, n_layers, activation, beta)
-reducer.train_vae(n_epochs)
+if __name__ == 'main':
+    main()
 
 
 
-# Interpolator's params' grid
-interpolator_grid = {
-    'smoothing': np.linspace(0.001, 1.0, num=10),
-    'kernel': ['multiquadric', 'inverse_multiquadric', 'inverse_quadratic', 'gaussian', 'linear', 'quintic', 'cubic', 'thin_plate_spline'],
-    'epsilon': np.linspace(0.0, 3.0, num=30)
-}
 
-param_combinations = list(itertools.product(*interpolator_grid.values()))
 
-best_validation_distance = float('inf')
-best_params = None
 
-# Loop over all combinations of hyperparameters
-param_combinations = list(itertools.product(*interpolator_grid.values()))
 
-best_validation_distance = float('inf')
-best_params = None
 
-for i, params in enumerate(param_combinations):
-    # unpack params
-    smoothing, kernel, epsilon = params
 
-    # train the model
-    #original_data = df.drop(['ID', 'PRESET_NAME'], axis=1)
-    original_data = df.drop(['ID', 'name', 'file'], axis=1)
-    original_data = original_data.values
-    reduced_data, _ = reducer.vae()
-    reduced_data = reduced_data[:, 1:]
-    # train the interpolator
-    interpolator = RBFInterpolator(reduced_data, original_data, smoothing=smoothing, kernel=kernel, epsilon=epsilon)
-    interpolated_data = interpolator(reduced_data)
 
-    # Compute euclidean distance between original and reduced vectors
-    distances = []
-    for original, interpolated in zip(original_data, interpolated_data):
-        distance = euclidean(original, interpolated)
-        distances.append(distance)
-
-    validation_distance = np.mean(distances)
-
-    print(f'Trial {i+1}/{len(param_combinations)}: validation_distance = {validation_distance}')
-
-    if validation_distance < best_validation_distance:
-        best_validation_distance = validation_distance
-        best_params = params
-
-# Save best VAE params and log the best hyperparameters and validation error
-best_rbf_params = best_params
-logging.info(f'Best RBF params: {best_rbf_params} with a validation error of {best_validation_distance}')
-
-#TODO: add a flag to select optim routine: vae, rbf, both
